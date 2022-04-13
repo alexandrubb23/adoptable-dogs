e12dbe35c661221a99cce871f4062e107f9f58e8
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2022-04-13 16:54:38 +0300

    Remove console log.

--- src/api.ts
+++ src/api.ts
@@ -3,7 +3,7 @@ import { QueryClient } from 'react-query';
 
 import { getSdk } from '../src/generated/graphql';
 import { API_URL } from '../src/config/constants';
-console.log({ API_URL });
+
 const gqlClient = new GraphQLClient(`${API_URL}/graphql`);
 export const { getDogs, dogByName } = getSdk(gqlClient);
 
