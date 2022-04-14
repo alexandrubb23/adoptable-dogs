d12e75526fe70b17714026bf0c68617213aa02fc
Barbulescu Alexandru <alex_bb23@yahoo.co.uk>
2022-04-14 13:16:10 +0300

    Add API_URL to next config env.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-module.exports = () => {
-  return {
-    reactStrictMode: true,
-    webpack: config => {
-      if (!config.experiments) {
-        config.experiments = {};
-      }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  reactStrictMode: true,
+  webpack: config => {
+    if (!config.experiments) {
+      config.experiments = {};
+    }
 
-      config.experiments.topLevelAwait = true;
-      return config;
-    },
-  };
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
 };
+
+module.exports = nextConfig;
